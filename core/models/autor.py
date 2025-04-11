6facd3eea4d76c5166f38382aa04249f11a18fcb
gabrielmartinsgomes <gabrielhenrickmartind3454@gmail.com>
2025-04-11 15:34:32 -0300

    FEAT: Criação da API para LIvros

--- core/models/autor.py
+++ core/models/autor.py
@@ -8,6 +8,10 @@ class Autor(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, blank=True, null=True)
 
+    def __str__(self):
+        return f'({self.id}) {self.nome}'
+    
+
     class Meta:
         verbose_name = "Autor"
         verbose_name_plural = "Autores"
