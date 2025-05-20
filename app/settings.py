59ae5a4e0db3857fd6f9090adb6b4c59cacc45cc
gabrielmartinsgomes <gabrielhenrickmartind3454@gmail.com>
2025-05-20 08:01:01 -0300

    fix: tentativa deploy pro render

--- app/settings.py
+++ app/settings.py
@@ -118,6 +118,7 @@ FILE_UPLOAD_PERMISSIONS = 0o640
 if MODE == 'DEVELOPMENT':
     MY_IP = os.getenv('MY_IP', '127.0.0.1')
     MEDIA_URL = f'http://{MY_IP}:19003/media/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 else:
     MEDIA_URL = '/media/'
     CLOUDINARY_URL = os.getenv('CLOUDINARY_URL')
