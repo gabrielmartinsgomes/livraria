c93eda76b59bcbbb34a5d8af940f048b1f5e7f39
gabrielmartinsgomes <gabrielhenrickmartind3454>
2025-08-29 14:38:44 -0300

    feat: validando a quantidade de itens na compra

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -4,9 +4,10 @@ from rest_framework.serializers import (
     HiddenField,         # novo
     ModelSerializer,
     SerializerMethodField,
+    ValidationError,
 )
 
-from core.models import Compra, ItensCompra
+from core.models import Compra, ItensCompra, Livro
 
 class ItensCompraSerializer(ModelSerializer):
     total = SerializerMethodField()
@@ -24,6 +25,15 @@ class ItensCompraCreateUpdateSerializer(ModelSerializer):
         model = ItensCompra
         fields = ('livro', 'quantidade')
 
+    def validate_quantidade(self, quantidade):
+        if quantidade <= 0:
+            raise ValidationError('A quantidade deve ser maior do que zero.')
+        return quantidade
+    def validate(self, item):
+        if item['quantidade'] > item['livro'].quantidade:
+            raise ValidationError('Quantidade de itens maior do que a quantidade em estoque.')
+        return item
+
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
     status = CharField(source='get_status_display', read_only=True)
@@ -35,6 +45,7 @@ class CompraSerializer(ModelSerializer):
         
 class CompraCreateUpdateSerializer(ModelSerializer):
     usuario = HiddenField(default=CurrentUserDefault())
+    itens = ItensCompraCreateUpdateSerializer(many=True)
 
     class Meta:
         model = Compra
@@ -42,11 +53,12 @@ class CompraCreateUpdateSerializer(ModelSerializer):
 
     def create(self, validated_data):
         itens_data = validated_data.pop('itens')
-        compra = Compra.objects.create(**validated_data)
+        usuario = validated_data.pop('usuario')  # vem do HiddenField
+        compra = Compra.objects.create(usuario=usuario, **validated_data)
         for item_data in itens_data:
             ItensCompra.objects.create(compra=compra, **item_data)
-        compra.save()
         return compra
+
     def update(self, compra, validated_data):
         itens_data = validated_data.pop('itens', [])
         if itens_data:
@@ -54,6 +66,7 @@ class CompraCreateUpdateSerializer(ModelSerializer):
             for item_data in itens_data:
                 ItensCompra.objects.create(compra=compra, **item_data)
         return super().update(compra, validated_data)
+
 
 
 
--- core/views/compra.py
+++ core/views/compra.py
@@ -1,16 +1,33 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from core.models import Compra
-from core.serializers import CompraCreateUpdateSerializer, CompraListSerializer, CompraSerializer
+from core.serializers import (
+    CompraCreateUpdateSerializer,
+    CompraListSerializer,
+    CompraSerializer,
+)
+
 
 class CompraViewSet(ModelViewSet):
     queryset = Compra.objects.all()
-    serializer_class = CompraSerializer
-    
+    permission_classes = [IsAuthenticated]
+
     def get_queryset(self):
         usuario = self.request.user
         if usuario.is_superuser:
             return Compra.objects.all()
-        if usuario.groups.filter(name='administradores'):
+        if usuario.groups.filter(name='administradores').exists():
             return Compra.objects.all()
         return Compra.objects.filter(usuario=usuario)
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return CompraCreateUpdateSerializer
+        if self.action == 'list':
+            return CompraListSerializer
+        return CompraSerializer
+
+    def perform_create(self, serializer):
+        # garante que sempre salva o usuário logado
+        serializer.save(usuario=self.request.user)
